e0d72e34e2f49b60ad6ab9d9edc31668a117fbf4
skdding3 <skdding3@gmail.com>
2022-05-09 16:11:25 +0900

    라우터 기본 구축

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+export default function Profile(props) {
+  const id = props.match.params.id;
+  console.log(id, typeof id);
+  return (
+    <div>
+      <h2>Profile 입니다.</h2>
+      {id && <p>id는 {id}입니다.</p>}
+    </div>
+  );
+}
